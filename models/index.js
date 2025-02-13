d3e5a017b3a10158aa80203767ab612cd477f6d3
Mehedi Hasan <130404728+Mehedi-haasan@users.noreply.github.com>
2025-02-13 21:06:01 +0600

    add

--- models/index.js
+++ models/index.js
@@ -1,78 +1,75 @@
 const config = require("../config/db.config");
+const { Sequelize } = require("sequelize");
+const mysql2 = require("mysql2"); // Ensure mysql2 is required
 
-const Sequelize = require("sequelize");
+// Initialize Sequelize
+const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
+  host: config.HOST,
+  dialect: config.dialect,
+  dialectModule: mysql2, // Ensure mysql2 is used
+  pool: {
+    max: config.pool.max,
+    min: config.pool.min,
+    acquire: config.pool.acquire,
+    idle: config.pool.idle
+  },
+  logging: false, // Disable logging for cleaner output
+});
 
-const sequelize = new Sequelize(
-  config.DB,
-  config.USER,
-  config.PASSWORD,
-  {
-    host: config.HOST,
-    dialect: config.dialect,
-    pool: {
-      max: config.pool.max,
-      min: config.pool.min,
-      acquire: config.pool.acquire,
-      idle: config.pool.idle
-    },
-    logging: false
-  }
-);
-
+// Create `db` object
 const db = {};
-
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
+// Import models
 db.user = require("./user.model")(sequelize, Sequelize);
 db.role = require("./role.model")(sequelize, Sequelize);
-db.productTemplete = require("../models/ProductModel")(sequelize, Sequelize);
+db.productTemplete = require("./ProductModel")(sequelize, Sequelize);
 db.saleorder = require("./saleorder.model")(sequelize, Sequelize);
 db.company = require("./company.model")(sequelize, Sequelize);
 db.message = require("./message.model")(sequelize, Sequelize);
 db.state = require("./state.model")(sequelize, Sequelize);
-db.category = require("./category")(sequelize, Sequelize);
-db.userdue = require("./userDue")(sequelize, Sequelize);
+db.category = require("./category.model")(sequelize, Sequelize);
+db.userdue = require("./userDue.model")(sequelize, Sequelize);
 
-// relation between tables
-
-// Sale Order
+// Sale Order Relationship
 db.productTemplete.hasMany(db.saleorder, {
   foreignKey: "product_id",
   onDelete: 'CASCADE',
-})
+});
 db.saleorder.belongsTo(db.productTemplete, {
   foreignKey: "product_id",
   onDelete: 'CASCADE',
-})
+});
 
-// State
+// State Relationship
 db.user.hasMany(db.state, {
   foreignKey: "userId",
   onDelete: 'CASCADE',
-})
+});
 db.state.belongsTo(db.user, {
   foreignKey: "userId",
   onDelete: 'CASCADE',
-})
-// SaleOrder
+});
+
+// Sale Order Relationship
 db.user.hasMany(db.saleorder, {
   foreignKey: "userId",
   onDelete: 'CASCADE',
-})
+});
 db.saleorder.belongsTo(db.user, {
   foreignKey: "userId",
   onDelete: 'CASCADE',
-})
+});
 
-// UserDue
+// User Due Relationship
 db.user.hasMany(db.userdue, {
   foreignKey: "userId",
   onDelete: 'CASCADE',
-})
+});
 db.userdue.belongsTo(db.user, {
   foreignKey: "userId",
   onDelete: 'CASCADE',
-})
+});
 
 module.exports = db;
